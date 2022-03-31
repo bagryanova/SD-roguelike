0894255142a3f05d978044f0fcfd615dbfe0be5d
OlesiaSub <lesya.sub@mail.ru>
2022-03-31 04:42:08 +0300

    Fix java 11 usage

--- src/main/java/ru/hse/sd/roguegue/logic/UserAction.java
+++ src/main/java/ru/hse/sd/roguegue/logic/UserAction.java
@@ -6,15 +6,20 @@ import ru.hse.sd.roguegue.status.Status;
 public class UserAction {
     /**
      * Updates user state according to the move made by user
+     *
      * @param move current user's move
      */
     public void updateState(Move move) {
         Position position = Status.userState.getPosition();
         switch (move) {
-            case UP -> position.updatePosition(position.getX(), position.getY() + 1);
-            case DOWN -> position.updatePosition(position.getX(), position.getY() - 1);
-            case LEFT -> position.updatePosition(position.getX() - 1, position.getY());
-            case RIGHT -> position.updatePosition(position.getX() + 1, position.getY());
+            case UP:
+                position.updatePosition(position.getX(), position.getY() + 1);
+            case DOWN:
+                position.updatePosition(position.getX(), position.getY() - 1);
+            case LEFT:
+                position.updatePosition(position.getX() - 1, position.getY());
+            case RIGHT:
+                position.updatePosition(position.getX() + 1, position.getY());
         }
     }
 }
