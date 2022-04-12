482a8cfa8238047da68c02964cf42c27fd60cfe6
OlesiaSub <lesya.sub@mail.ru>
2022-04-12 19:35:32 +0300

    Remove commented code

--- src/main/java/ru/hse/sd/roguegue/map/util/RandomMapGeneratorUtil.java
+++ src/main/java/ru/hse/sd/roguegue/map/util/RandomMapGeneratorUtil.java
@@ -2,8 +2,6 @@ package ru.hse.sd.roguegue.map.util;
 
 import ru.hse.sd.roguegue.map.CellType;
 import ru.hse.sd.roguegue.map.Map;
-import ru.hse.sd.roguegue.state.Position;
-import ru.hse.sd.roguegue.status.Status;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,25 +27,6 @@ public class RandomMapGeneratorUtil {
         connectGroundSpaces(groundSpaces);
         GroundSpace gs = groundSpaces.get(rand.nextInt(groundSpaces.size()));
         cells[gs.x() + 2][gs.y() + 2] = CellType.EXIT;
-        boolean setPosition = true;
-        CellType[][] borderedCells = new CellType[cells.length + 2][cells[0].length + 2];
-//        for (int i = 0; i < borderedCells.length; i++) {
-//            for (int j = 0; j < borderedCells[0].length; j++) {
-//                if (i == 0 || i == borderedCells.length - 1 || j == 0 || j == borderedCells[0].length - 1) {
-//                    borderedCells[i][j] = CellType.OBSTACLE;
-//                } else {
-//                    borderedCells[i][j] = cells[i - 1][j - 1];
-//                }
-//            }
-//        }
-//        for (int i = 0; i < borderedCells.length; i++) {
-//            for (int j = 0; j < borderedCells[0].length; j++) {
-//                if (borderedCells[i][j] == CellType.GROUND && setPosition) {
-//                    setPosition = false;
-//                    Status.userState.updatePosition(new Position(j, i));
-//                }
-//            }
-//        }
         CommonMapUtil commonMapUtil = new CommonMapUtil();
         return new Map(commonMapUtil.setBoundsAndPositions(cells));
     }
