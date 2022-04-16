07e2cf03868f4a167fecc740113c20ecbb833d0e
Karina5005 <anisimova5005@gmail.com>
2022-04-16 16:27:29 +0300

    fix inventory handler

--- src/main/java/ru/hse/sd/roguegue/preprocessing/InventoryHandler.java
+++ src/main/java/ru/hse/sd/roguegue/preprocessing/InventoryHandler.java
@@ -10,7 +10,7 @@ public class InventoryHandler {
     private final InventoryAction inventoryAction = new InventoryAction();
 
     private boolean validateAction(KeyEvent event) {
-        return event.getKeyCode() == KeyEvent.VK_Q || event.getKeyCode() == KeyEvent.VK_ENTER || event.getKeyCode() == KeyEvent.VK_UP || event.getKeyCode() == KeyEvent.VK_DOWN || event.getKeyCode() == KeyEvent.VK_DELETE;
+        return event.getKeyCode() == KeyEvent.VK_Q || event.getKeyCode() == KeyEvent.VK_ENTER || event.getKeyCode() == KeyEvent.VK_UP || event.getKeyCode() == KeyEvent.VK_DOWN || event.getKeyCode() == KeyEvent.VK_BACK_SPACE;
     }
 
     public void setInventoryAction(KeyEvent event) {
@@ -21,7 +21,7 @@ public class InventoryHandler {
             case KeyEvent.VK_UP -> inventoryAction.updateState(Move.UP);
             case KeyEvent.VK_DOWN -> inventoryAction.updateState(Move.DOWN);
             case KeyEvent.VK_ENTER -> inventoryAction.updateState(Move.PUT_ON);
-            case KeyEvent.VK_DELETE -> inventoryAction.updateState(Move.TAKE_OFF);
+            case KeyEvent.VK_BACK_SPACE -> inventoryAction.updateState(Move.TAKE_OFF);
             case KeyEvent.VK_Q -> inventoryAction.updateState(Move.ENTER);
         }
     }
